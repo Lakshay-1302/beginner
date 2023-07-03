22ffaf40c2d0249ff5b3fd6d1fd3efb6c30ed375
Lakshay-1302 <138498812+Lakshay-1302@users.noreply.github.com>
2023-07-03 19:20:41 +0530

    added my 2 programs of C

---
+++ binary.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int opcount=0;
+
+int main(){
+    int n,i,j,key;
+    printf("enter the elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter the array: ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("enter the element to be searched: ");
+    scanf("%d",&key);
+    int low=0,high=n-1,mid;
+    do{
+        mid=floor((low+high)/2);
+        if(key<a[mid]){
+            high=mid-1;
+        }
+        else if(key>a[mid]){
+            low=mid+1;
+        }
+    }while(key!=a[mid] && low<=high);
+
+    if(key==a[mid]){
+        printf("element found at position %d",mid+1);
+    }
+    else{
+        printf("element not found");
+    }
+}
